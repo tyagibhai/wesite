d5ce7cc2b3b39261004cc6d99a002266032bea95
Anil Sharma <anilsharma@Anils-MacBook-Air.local>
2020-10-08 12:29:33 +0530

    minar css fixes

--- resources/views/livewire/pages/search/search-header.blade.php
+++ resources/views/livewire/pages/search/search-header.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <section class="hero-banner hero-banner--sm mb-30px">
+    <section class="hero-banner hero-banner--sm mb-30px search-header-cont">
         <div class="container">
             <div class="hero-banner--sm__content">
                 <h1>{{ $page_title }}</h1>
